542e8552ca8f9355aeadf26c1da7db37abba0eea
d-wack <dotwack@pm.me>
2025-04-30 12:42:20 +0300

    Added Amplify Outputs

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// import Amplify component from 'aws-amplify' this allows the application to use AWS Amplify
+import { Amplify } from 'aws-amplify';
+import outputs from "../amplify_outputs.json";
 
+Amplify.configure(outputs);
+//---------------------------------------------------------
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
